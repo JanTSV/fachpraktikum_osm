bc65fb4769b1d43e2495267312bec94286c4df48
JanTSV <jantheil2001@gmail.com>
2025-10-23 00:38:09 +0200

    display all buildings as markers

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 #include <chrono>
 #include <iomanip>
 
+#include <optional>
 #include <osmium/io/any_input.hpp>
 #include <osmium/handler.hpp>
 #include <osmium/visitor.hpp>
 #include <string>
+#include <vector>
 
 #include "httplib.h"
 
@@ -34,42 +36,73 @@ std::string get_duration(const Duration& duration) {
 }
 
 struct Point {
-    private:
-        double _x;  // lat
-        double _y;  // lon
-
     public:
-        Point(double x, double y) : _x(x), _y(y) {
+        double x;  // lat
+        double y;  // lon
+        Point(double x, double y) : x(x), y(y) {
 
         }
 
         double euclidean_distance(Point& other) {
-            return std::sqrt(std::pow(_x - other._x, 2) + std::pow(_y - other._y, 2));
+            return std::sqrt(std::pow(x - other.x, 2) + std::pow(y - other.y, 2));
         }
 };
 
 struct Building {
-    private:
-        Point _location;
-        size_t _street_idx;
+    public:
+        Point location;
+        std::optional<size_t> street_idx;
         // TODO: house number
 
-    public:
-        Building(Point location, size_t street_idx) : _location(location), _street_idx(street_idx) {
+        Building(Point location, std::optional<size_t> street_idx) : location(location), street_idx(street_idx) { }
+};
 
+struct NaiveStringStore {
+    private:
+        std::vector<std::string> _data;
+
+    public:
+        size_t get_or_add(std::string value) {
+            // Very naive string search in O(m*n)
+            for (size_t i = 0; i < _data.size(); i++) {
+                if (value == _data[i]) {
+                    return i;
+                }
+            }
+
+            _data.push_back(value);
+            return _data.size() - 1;
         }
 };
 
+// TODO: IStringStore and IBuildingStore in here
 class ISolution {
     public:
         virtual ~ISolution() {}
-        virtual void add_building(Building building) = 0;
+        virtual void add_building(double lat, double lon, const char* street) = 0;
+        virtual const std::vector<Building>& get_buildings() const = 0;
 };
 
 class NaiveSolution : public ISolution {
-    public:
-        void add_building(Building building) override {
+    private:
+        NaiveStringStore string_store;
+        std::vector<Building> buildings;
 
+    public:
+        void add_building(double lat, double lon, const char* street) override {
+            // Store street name, it building has one tagged
+            std::optional<size_t> street_idx;
+            if (street) {
+                street_idx = string_store.get_or_add(street);
+            }
+            // onstruct Building and add to vector
+            Point location{lat, lon};
+            buildings.emplace_back(location, street_idx);
+        }
+
+
+        const std::vector<Building>& get_buildings() const override {
+            return buildings;
         }
 };
 
@@ -92,7 +125,11 @@ class OSMHandler : public osmium::handler::Handler {
                     return;
                 }
 
-                std::cout << "Node ID: " << node.id() << std::endl;
+                const char* street = tags.get_value_by_key("addr:street");
+                // TODO: const char* housenumber = tags.get_value_by_key("addr:housenumber");
+
+                // TODO: Many dont have a street name
+                _solution.add_building(node.location().lat(), node.location().lon(), street);
             }
         }
 
@@ -103,11 +140,41 @@ class OSMHandler : public osmium::handler::Handler {
                 tags.get_value_by_key("shop") ||
                 tags.get_value_by_key("amenity")) {
                 if (!way.is_closed()) {
+                    // std::cerr << "WARNING: Building is not a closed way." << std::endl;
                     return;
                 }
 
-                std::cout << "Way ID: " << way.id() << std::endl;
+                const char* street = tags.get_value_by_key("addr:street");
+                // TODO: const char* housenumber = tags.get_value_by_key("addr:housenumber");
+                
+                // Compute centroid
+                double sum_lat = 0.0, sum_lon = 0.0;
+                size_t count = 0;
+                const auto& nodes = way.nodes();
+
+                // Can skip first node since its a closed way
+                for (size_t i = 1; i < nodes.size(); ++i) {
+                    const auto& node_ref = nodes[i];
+                    if (node_ref.location().valid()) {
+                        sum_lat += node_ref.location().lat();
+                        sum_lon += node_ref.location().lon();
+                        ++count;
+                    }
+                }
+
+                if (count > 0) {
+                    // TODO: Many dont have a street name (maybe a node in the list has?)
+                    _solution.add_building(sum_lat / count, sum_lon / count, street);
+                } else {
+                    // std::cerr << "WARNING: One way consists of 0 nodes." << std::endl;
+                }
             }
+        }
+
+        void relation(const osmium::Relation& rel) {
+            if (!rel.tags().get_value_by_key("building")) return;
+
+            // TODO: Relations can be buildings.
         }
 };
 
@@ -126,12 +193,25 @@ int main(int argc, char* argv[]) {
     reader.close();
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "Parsing done " << get_duration(end - start) << std::endl;
+    std::cout << "NUmber of buildings: " << solution.get_buildings().size() << std::endl;
 
     httplib::Server svr;
     svr.set_mount_point("/", "./www");
 
-    // TODO: API endpoint for cities
-    svr.Get("/points", [](const httplib::Request&, httplib::Response& res) {
+    // API endpoint for cities
+    svr.Get("/buildings", [&solution](const httplib::Request&, httplib::Response& res) {
+        const auto& buildings = solution.get_buildings();
+        std::ostringstream json;
+
+        json << "[";
+        for (size_t i = 0; i < buildings.size(); ++i) {
+            const auto& b = buildings[i];
+            json << "[" << b.location.x << "," << b.location.y << "]";
+            if (i + 1 < buildings.size()) json << ",";
+        }
+        json << "]";
+
+        res.set_content(json.str(), "application/json");
     });
 
     std::cout << "Server started at http://localhost:" << PORT << std::endl;
